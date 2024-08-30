115d0b6ab1f238f9392daed55bb1273f06b56bc9
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-30 18:33:55 +0200

    menu

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-//funzione per il toggle del menu
+// funzione per abbassare il panel (che ha class .modify)
+const closeModifyButton = document.querySelector(".main__close");
 
-let nav = document.querySelector("nav");
-let toggleMenu = document.getElementById("hamburger");
+let closeModify = function () {
+  let modify = document.querySelector(".modify");
+  modify.style.bottom = "-300px";
+  closeModifyButton.innerHTML = "";
+};
 
-toggleMenu.addEventListener("click", function () {
-  toggleMenu.innerHTML.toggle('<i class="fas fa-window-close"></i>');
+closeModifyButton.addEventListener("click", closeModify);
+
+// funzione per mettere/togliere il menu a lato
+
+const hamburger = document.getElementById("hamburger");
+hamburger.addEventListener("click", function () {
+  let nav = document.querySelector("nav");
+  if (hamburger.innerHTML === `<i class="fas fa-bars"></i>`) {
+    nav.style.left = "-90%";
+    hamburger.innerHTML = `<i class="fas fa-times"></i>`;
+    hamburger.style.color = "red";
+  } else {
+    nav.style.left = "-104%";
+    hamburger.innerHTML = `<i class="fas fa-bars"></i>`;
+    hamburger.style.color = "rgb(117, 117, 117)";
+  }
 });
