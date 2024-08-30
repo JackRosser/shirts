37bdd834045b65ceb2c77d9f0827f3e84cdf40fd
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-30 19:07:59 +0200

    vai

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ hamburger.addEventListener("click", function () {
     hamburger.style.color = "rgb(117, 117, 117)";
   }
 });
+
+{
+  /* <form id="color__modify" class="modify">
+<div>
+  <!-- questo è la X di chiusura dentro il button -->
+  <div class="main__close"><i class="fas fa-times"></i></div>
+  <!-- _______________________________________ -->
+  <label for="shirt__color"></label>
+  <input type="color" id="shirt__color" />
+  <span id="shirt__icon"><i class="fas fa-tshirt"></i></span>
+</div>
+<button type="submit" id="ok__button">OK</button>
+</form> */
+}
+
+let coloredMiniShirt = document.getElementById("shirt__icon");
+let colorPanel = document.getElementById("shirt__color");
+let colorBigShirt = document.getElementById("base__shirt");
+
+colorPanel.addEventListener("input", function () {
+  coloredMiniShirt.style.color = colorPanel.value;
+  colorBigShirt.style.backgroundColor = colorPanel.value;
+});
+
+// Ritorna al colore originale se l'utente clicca fuori o non conferma
+// colorPanel.addEventListener("change", function () {
+//   colorBigShirt.style.backgroundColor = `rgb(210, 210, 210)`;
+// });
