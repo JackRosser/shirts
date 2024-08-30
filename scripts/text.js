20bb9ec7074084fc2fa1af7ab84fd3b7bd61a187
Giacomo Rossettini <rossegiac@gmail.com>
2024-08-31 01:45:59 +0200

    ok

--- scripts/text.js
+++ scripts/text.js
@@ -12,35 +12,67 @@ textArea.addEventListener("input", function () {
 // bottoncini per modificare lo stile
 
 let font = document.getElementById("font");
-let fontColor = document.getElementById("font-color");
-let justify = document.getElementById("justify");
 
 let divCreation = function () {
+  let existingDiv = document.querySelector(".font-modify");
+  if (existingDiv) {
+    existingDiv.remove(); // Rimuove il div esistente prima di crearne uno nuovo
+  }
+
   let main = document.querySelector("main");
   let div = document.createElement("div");
   div.className = "font-modify";
+
+  // Creazione del pulsante di chiusura
+  let close = document.createElement("div");
+  close.innerHTML = `<i class="fas fa-times"></i>`;
+  close.className = "petit__close";
+  div.appendChild(close);
+
+  // Aggiungi l'evento di chiusura
+  close.addEventListener("click", function () {
+    div.remove(); // Rimuove il div quando si clicca sulla X
+  });
+
   main.appendChild(div);
+  return div;
 };
 
 font.addEventListener("click", function () {
-  divCreation();
+  let div = divCreation();
   let select = document.createElement("select");
   select.id = "options";
+
   let fontDefault = document.createElement("option");
   fontDefault.value = "roboto";
   fontDefault.innerText = "Default (Roboto)";
+  fontDefault.className = "menu_tendina";
+  fontDefault.style.fontFamily = `"Roboto", sans-serif`;
+
   let rock = document.createElement("option");
   rock.value = "Rock Salt";
   rock.innerText = "Rock Salt";
+  rock.className = "menu_tendina";
+  rock.style.fontFamily = `"Rock Salt", cursive`;
+
   let comic = document.createElement("option");
   comic.value = "Comic Neue";
   comic.innerText = "Comic Neue";
+  comic.className = "menu_tendina";
+  comic.fontFamily = `"Comic Neue", cursive`;
+
   let chewy = document.createElement("option");
   chewy.value = "Chewy";
   chewy.innerText = "Chewy";
+  chewy.className = "menu_tendina";
+  chewy.fontFamily = `"Chewy", system-ui`;
+
   let bangers = document.createElement("option");
   bangers.value = "Bangers";
   bangers.innerText = "Bangers";
+  bangers.className = "menu_tendina";
+  bangers.fontFamily = `"Bangers", system-ui`;
+
   select.append(fontDefault, rock, comic, chewy, bangers);
-  div.append(select);
+  div.appendChild(select);
 });
